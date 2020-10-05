bcd408c6e19030146541c2558f52448536ad91b7
alekhya87 <davulurialekhya87909.com>
2020-10-05 16:42:55 +0530

    employee attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,10 +3,8 @@ isPresent=1;
 randomCheck=$((RANDOM%2))
 if [ $isPresent -eq $randomCheck ]
 then
-   empRatePerHr=20
-   empHrs=8
-   salary=$(($empHrs*$empRatePerHr))
+   echo "present"
 else
-salary=0
+	echo "absent"
 fi
 
